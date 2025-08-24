47c4a8c24c036699d9ea23c7e05c704a4e66a5a4
HongyiYoung <152992193+HongyiYoung@users.noreply.github.com>
2025-08-24 07:14:31 +0000

    Deploying to gh-pages from @ HongyiYoung/HongyiYoung.github.io@a3dbe8ca8e53400e6c6d653e3bbd9a21b96ad09d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,13 +448,6 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://vinyyang.github.io", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
