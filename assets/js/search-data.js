deec7bc07b244f361b5d6e10d4c03b67712c305f
HongyiYoung <152992193+HongyiYoung@users.noreply.github.com>
2026-06-02 04:27:13 +0000

    Deploying to gh-pages from @ HongyiYoung/HongyiYoung.github.io@5ebd06a36b756b63489cb107b3f1a3962f81e77a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -493,25 +493,11 @@ ninja.data = [{
           window.open("", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-cv_pdf',
+        title: 'Cv_pdf',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("", "_blank");
         },
       },{
       id: 'light-theme',
